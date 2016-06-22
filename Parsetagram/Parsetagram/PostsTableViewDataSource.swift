4758a5404f194de3afb02da50f24509b4c67f017
Yijin Kang <yijinkang@gmail.com>
2016-06-21 19:06:03 -0700

    I tried but not Posts isn't even working

---
+++ Parsetagram/Parsetagram/PostsTableViewDataSource.swift
@@ -0,0 +1,13 @@
+//
+//  PostsTableViewDataSource.swift
+//  Parsetagram
+//
+//  Created by Yijin Kang on 6/21/16.
+//  Copyright © 2016 Yijin Kang. All rights reserved.
+//
+
+import Cocoa
+
+class PostsTableViewDataSource: UITableViewDataSource {
+
+}
---
+++ Parsetagram/Parsetagram/util.swift
@@ -0,0 +1,9 @@
+//
+//  util.swift
+//  Parsetagram
+//
+//  Created by Yijin Kang on 6/21/16.
+//  Copyright © 2016 Yijin Kang. All rights reserved.
+//
+
+import Foundation
